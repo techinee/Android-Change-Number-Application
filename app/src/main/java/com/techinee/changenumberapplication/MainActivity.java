a730a7b361c4dd8403ca6e8e12edaf1c0c318ff6
techinee <6206021621066@fitm.kmutnb.ac.th>
2022-01-20 17:34:24 +0700

    finish

--- app/src/main/java/com/techinee/changenumberapplication/MainActivity.java
+++ app/src/main/java/com/techinee/changenumberapplication/MainActivity.java
@@ -3,12 +3,38 @@ package com.techinee.changenumberapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText decText;
+    private Button btnCal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        decText = (EditText) findViewById(R.id.desText);
+        btnCal = (Button) findViewById(R.id.btnCal);
+
+        btnCal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int dec;
+                dec = Integer.parseInt(decText.getText().toString());
+                String binText = ""; //binary text
+                binText = decToBin(dec);
+                Toast.makeText(MainActivity.this , binText , Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private String decToBin(int dec){
+        String binary = Integer.toBinaryString(dec);
+        return ("Binary Number : " + binary);
     }
 }
